f1fa1af9d43d3c42b46c9a1ea5dc6e8f27c4eb7b
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2021-08-31 16:31:40 -0300

    Update

--- web/src/pages/CreateRoom/index.tsx
+++ web/src/pages/CreateRoom/index.tsx
@@ -7,7 +7,9 @@ import logoUenp from '../../assets/UenpLogoPequena.png';
 
 import './style.css';
 import { Sidebar } from '../../components/Sidebar';
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import api from '../../services/api';
+import { useHistory } from 'react-router-dom';
 
 const happyMapIcon = L.icon({
   iconUrl: logoUenp,
@@ -18,8 +20,16 @@ const happyMapIcon = L.icon({
 })
 
 export function CreateRoom() {
+  const history = useHistory();
+
   const [position, setPosition] = useState({ latitude: 0, longitude: 0 })
 
+  const [name, setName] = useState('');
+  const [campus, setCampus] = useState('');
+  const [description, setDescription] = useState('');
+  const [weight, setWeight] = useState('');
+  const [images, setImages] = useState<File[]>([]);
+  const [previewImages, setPreviewImages] = useState<string[]>([]);
 
   function handleMapClick(event: LeafletMouseEvent) {
     const { lat, lng } = event.latlng;
@@ -30,11 +40,51 @@ export function CreateRoom() {
     });
   }
 
+  function handleSelectImage(event: ChangeEvent<HTMLInputElement>) {
+    if(!event.target.files) {
+      return;
+    }
+    
+    const selectedImages = Array.from(event.target.files)
+
+    setImages(selectedImages);
+
+    const selectedImagesPreview = selectedImages.map(image => {
+      return URL.createObjectURL(image);
+    });
+
+    setPreviewImages(selectedImagesPreview);
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const { latitude, longitude } = position;
+
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('campus', campus);
+    data.append('description', description);
+    data.append('weight', weight);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    images.forEach(image => {
+      data.append('images', image);
+    })
+
+    await api.post('/room', data);
+
+    alert('Cadastro realizado com sucesso!');
+
+    history.push('/');
+  }
+
   return (
     <div id="page-create-room">
       <main>
         <Sidebar />
-        <form className="create-room-form">
+        <form className="create-room-form" onSubmit={handleSubmit}>
           <fieldset>
             <legend>Cadastrar Sala</legend>
 
@@ -50,32 +100,49 @@ export function CreateRoom() {
 
               {
                 position.latitude !== 0 ?
-                 <Marker interactive={false} icon={happyMapIcon} position={[position.latitude, position.longitude]} /> :
-                 null
+                  <Marker interactive={false} icon={happyMapIcon} position={[position.latitude, position.longitude]} /> :
+                  null
               }
 
             </Map>
 
             <div className="input-block">
-              <label htmlFor="name">Nome</label>
-              <input id="name" />
+              <label htmlFor="name">Nome da Sala</label>
+              <input id="name" value={name} onChange={e => setName(e.target.value)} />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="name">Nome do Campus</label>
+              <input id="name" value={campus} onChange={e => setCampus(e.target.value)} />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="name">Peso</label>
+              <input id="name" value={weight} onChange={e => setWeight(e.target.value)} />
             </div>
 
             <div className="input-block">
               <label htmlFor="about">Descrição <span>Máximo de 300 caracteres</span></label>
-              <textarea id="name" maxLength={300} />
+              <textarea id="name" maxLength={300} value={description} onChange={e => setDescription(e.target.value)} />
             </div>
 
             <div className="input-block">
               <label htmlFor="images">Fotos</label>
 
-              <div className="uploaded-image">
+              <div className="images-container">
+                {previewImages.map(image => {
+                  return (
+                    <img key={image} src={image} alt={name} />
+                  ) 
+                })}
 
+                <label htmlFor="image[]" className="new-image">
+                  <FiPlus size={24} color="#0443d6" />
+                </label>
               </div>
 
-              <button className="new-image">
-                <FiPlus size={24} color="#0443d6" />
-              </button>
+              <input multiple onChange={handleSelectImage} type="file" name="" id="image[]" />
+
             </div>
           </fieldset>
           <button className="confirm-button" type="submit">
